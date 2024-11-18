a56dcb9de187267627a98f159a39403cb3e453a7
Krishna Mohan Singh <krishna.mohan@appinventiv.com>
2024-11-18 14:32:56 +0530

    launch the chat sdk from home screen

--- app/src/main/java/com/netomi/sampleapplication/ui/HomeActivity.kt
+++ app/src/main/java/com/netomi/sampleapplication/ui/HomeActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.netomi.chat.ui.init.NCWChatSdk
 import com.netomi.sampleapplication.R
 import com.netomi.sampleapplication.utils.AppSharedPreferences
 
@@ -18,6 +21,7 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var navView: NavigationView
     private lateinit var ivMenu: ImageView
     private lateinit var logout: AppCompatTextView
+    private lateinit var imgChat: AppCompatImageButton
     private lateinit var preferences :AppSharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +40,11 @@ class HomeActivity : AppCompatActivity() {
             startActivity(intent)
             finishAffinity()
         }
+
+        imgChat.setOnClickListener{
+            NCWChatSdk.launch(this,"b23963e4-56c5-4d8f-929e-2b0f1155b1f8")
+        }
+
     }
 
     // Initialize the views
@@ -45,5 +54,6 @@ class HomeActivity : AppCompatActivity() {
         ivMenu = findViewById(R.id.ivMenu)  // Find the ImageView for the menu icon
         navView = findViewById(R.id.navView)  // Find the NavigationView for the drawer content
         logout = findViewById(R.id.logout)  // Find the NavigationView for the drawer content
+        imgChat=findViewById(R.id.img_chat)
     }
 }
