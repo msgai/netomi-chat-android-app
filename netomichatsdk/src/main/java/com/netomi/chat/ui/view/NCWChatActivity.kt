da26ef5fdcaa34efa13842f68f26c9fcb17d06e8
Ram Naresh <ram.naresh1@appinventiv.com>
2024-11-25 16:18:57 +0530

    Fix [MCS-208] : Fix text color issue

--- netomichatsdk/src/main/java/com/netomi/chat/ui/view/NCWChatActivity.kt
+++ netomichatsdk/src/main/java/com/netomi/chat/ui/view/NCWChatActivity.kt
@@ -503,7 +503,7 @@ class NCWChatActivity : AppCompatActivity(), ChatActionCallback {
             }
 
             is State.Error -> {
-                Toast.makeText(this, "Error..", Toast.LENGTH_SHORT).show()
+              //  Toast.makeText(this, "Error..", Toast.LENGTH_SHORT).show()
             }
 
             else -> {
