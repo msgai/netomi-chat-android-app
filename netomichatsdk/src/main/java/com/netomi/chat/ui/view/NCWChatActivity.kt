1833408e149457fde38c416e1f5ea6e6dbfa4f48
Ram Naresh <ram.naresh1@appinventiv.com>
2025-02-20 13:12:28 +0530

    Fix issue(MCS -Issue)--Fix click issue and hint translation

--- netomichatsdk/src/main/java/com/netomi/chat/ui/view/NCWChatActivity.kt
+++ netomichatsdk/src/main/java/com/netomi/chat/ui/view/NCWChatActivity.kt
@@ -3539,6 +3539,7 @@ Log.e("ROUTE_SEND_TRANSCRIPT","Successss")
             tvBrandName.text=otherlocalized.powered_by_netomi
              headerTextView.text=languageResponse.title
             tvToday.text= otherlocalized.today
+            messageInputField.hint=otherlocalized.ask_a_question
         }
 
 
