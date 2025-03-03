1cc43531ff6426895d2a4b69affa1a571d605e88
Ram Naresh <ram.naresh1@appinventiv.com>
2025-03-03 16:45:56 +0530

    Fix issue((MCS-831))--Add current_sdk to custom attribute

--- netomichatsdk/src/main/java/com/netomi/chat/utils/NCWExtension.kt
+++ netomichatsdk/src/main/java/com/netomi/chat/utils/NCWExtension.kt
@@ -87,6 +87,12 @@ fun DeviceInfo.toNCWCustomAttributes(): ArrayList<NCWCustomAttribute> {
             value = deviceToken,
             scope = "LIFE_TIME"
         ),
+        NCWCustomAttribute(
+            type = "TEXT",
+            name = "current_sdk",
+            value = "mobile",
+            scope = "LIFE_TIME"
+        ),
 
         )
 
